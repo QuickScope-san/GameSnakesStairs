1b0d23025845aaeb221ad4cb0e9e9fadfadb9c78
QuickScope-san <kevin.cancimanse@correounivalle.edu.co>
2021-09-21 23:24:28 -0500

    Background Gui Added

--- src/gameSnakesStairs/GuiGameSnakesStairs.java
+++ src/gameSnakesStairs/GuiGameSnakesStairs.java
@@ -3,25 +3,91 @@ package gameSnakesStairs;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import javax.swing.*;
+
 import java.awt.Graphics;
 import java.awt.Image;
 
+import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.tools.JavaCompiler;
+
 public class GuiGameSnakesStairs extends JFrame {
+	
+	//Declaracion de constantes.
+	
+	//Declaracion de JButton's.
+	
+	//Declaracion de JLabel's.
+	private JLabel titulo; 
+	
+	//Declaracion de JPanel's.
+	
+	//Declaracion de Paneles auxiliares.
+	
+	//Declaracion de Esuchas.
+	
+	//Declaracion de la comuniacion con las otras clases.
+	
+	//Declaracion del objeto que contiene el fondo del juego
+	private FondoJuego fondo;
+	
+	//Declaracion del contenedor principal.
+	private Container contenedorPpal;
 	
 	
 	public GuiGameSnakesStairs() {
 		
+		//Instanciamiento de los objetos.
+		
 		initGuiGameSnakesStairs();
 		
+		//Creacion de ventana.
+		
+		this.setVisible(true);
 		this.setResizable(false);
 		this.setTitle("S E R P I E N T E S  Y  E S C A L E R A S");
-		this.setOpaque(false);
+		this.setSize(1200,700);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
 	
 	public void initGuiGameSnakesStairs() {
 		
+		//Creacion de JButton's.
 		
+		//Creacion de JLabel's.
+		titulo = new JLabel();
+		
+		//Creacion de Paneles.
+		
+		//Creacion de Paneles auxiliares.
+		
+		//Creacion de Esuchas.
+		
+		//Creacion de la comuniacion con las otras clases.
+		
+		//Creacion del objeto que contiene el fondo el juego.
+		fondo = new FondoJuego();
+		
+		//Creacion de Contenedor principal.
+		contenedorPpal = getContentPane();
+		
+		//Configuracion de gestores de diseño.
+		contenedorPpal.setLayout(new BorderLayout());
+		fondo.setLayout(new BorderLayout());
+		
+		//Agregacion de componentes/paneles a paneles mas grandes.
+		contenedorPpal.add(fondo, BorderLayout.CENTER);
+		
+		JOptionPane.showMessageDialog(null, "Hola");
 		
 	}
 
@@ -31,6 +97,8 @@ public class GuiGameSnakesStairs extends JFrame {
 		
 		@Override
 		public void paint(Graphics g) {
+			
+			//Configuracion de fondo.
 			
 			fondo = new ImageIcon(getClass().getResource("/imagenes/Fondo.png")).getImage();
 			g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
--- src/gameSnakesStairs/PrincipalMain.java
+++ src/gameSnakesStairs/PrincipalMain.java
@@ -1,9 +1,34 @@
 package gameSnakesStairs;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import java.awt.EventQueue;
+
 public class PrincipalMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+			
+		} catch (Exception e) {
+			
+			JOptionPane.showMessageDialog(null, "Error en maquina virtual debes re-instalar el IDE en cuestion");
+			
+		}
+		
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				GuiGameSnakesStairs juego = new GuiGameSnakesStairs();
+				
+			}
+			
+		});
 
 	}
 
